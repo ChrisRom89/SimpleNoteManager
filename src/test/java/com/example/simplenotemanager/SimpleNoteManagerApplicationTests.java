eff3d8416080830ed98555453245f576136cd1a1
Christian Romen <christian89rom@gmail.com>
2024-08-10 08:10:09 +0200

    build: add entry point and pom file to project

---
+++ src/test/java/com/example/simplenotemanager/SimpleNoteManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplenotemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNoteManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
